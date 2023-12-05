3049aca858df777aa183f55d80b6c6142da167f5
generiblora <g.riblora@arcanys.com>
2023-12-05 13:19:50 +0800

    updated calculator.js

--- tests/calculator.js
+++ tests/calculator.js
@@ -15,4 +15,19 @@ describe( 'demo calculator tests', function ()
 
         browser.sleep( 2000 );
     } );
+    
+    it( 'subtraction test', function ()
+    {
+        homepage.get( 'https://juliemr.github.io/protractor-demo/' );
+
+        homepage.enterFirstNumber( '4' );
+
+        homepage.enterSecondNumber( '3' );
+
+        homepage.clickGo1();
+
+        homepage.verifyResult( '7' );
+
+        browser.sleep( 2000 );
+    } );
 } );
